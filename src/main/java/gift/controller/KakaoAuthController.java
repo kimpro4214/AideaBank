25559bb17ac1fbadab39080a8090e880cf78424c
kimpro4214 <qnfghk555@naver.com>
2025-07-24 23:46:47 +0900

    feat: 인가 코드로 액세스 토큰 요청하는 KakaoAuthService 구현

---
+++ src/main/java/gift/controller/KakaoAuthController.java
@@ -0,0 +1,26 @@
+package gift.controller;
+
+import gift.service.KakaoAuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/kakao")
+public class KakaoAuthController {
+
+    private final KakaoAuthService kakaoAuthService;
+
+    public KakaoAuthController(KakaoAuthService kakaoAuthService) {
+        this.kakaoAuthService = kakaoAuthService;
+    }
+
+    @GetMapping("/callback")
+    public ResponseEntity<String> kakaoCallback(@RequestParam String code) {
+        var tokenResponse = kakaoAuthService.getAccessToken(code);
+        return ResponseEntity.ok("Access Token: " + tokenResponse.access_token());
+    }
+}
+
