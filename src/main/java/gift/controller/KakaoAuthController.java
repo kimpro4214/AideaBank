7a123e8861715eb192570651226f6d5a2aa9a1d0
kimpro4214 <qnfghk555@naver.com>
2025-08-01 05:00:58 +0900

    refactor: 카카오 로그인 주소 수정

--- src/main/java/gift/controller/KakaoAuthController.java
+++ src/main/java/gift/controller/KakaoAuthController.java
@@ -1,18 +1,39 @@
 package gift.controller;
 
+import gift.config.KakaoProperties;
 import gift.dto.KakaoTokenResponse;
 import gift.service.KakaoAuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/auth/kakao")
 public class KakaoAuthController {
 
     private final KakaoAuthService kakaoAuthService;
+    private final KakaoProperties properties;
 
-    public KakaoAuthController(KakaoAuthService kakaoAuthService) {
+
+    public KakaoAuthController(KakaoAuthService kakaoAuthService, KakaoProperties properties) {
         this.kakaoAuthService = kakaoAuthService;
+        this.properties = properties;
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<Void> redirectToKakaoLogin() {
+        URI redirectUri = URI.create(
+                "https://kauth.kakao.com/oauth/authorize" +
+                        "?client_id=" + properties.getClientId() +
+                        "&redirect_uri=" + properties.getRedirectUri() +
+                        "&response_type=code"
+        );
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(redirectUri);
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
     @GetMapping("/callback")
